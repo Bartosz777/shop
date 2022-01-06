e8bf80fe0106c23fc3cc3db334949794274c81c0
Bartek <bartekd001@gmail.com>
2022-01-06 12:59:25 +0100

    advertisment

--- src/products/components/Product.js
+++ src/products/components/Product.js
@@ -3,12 +3,19 @@ import React from 'react';
 
 import './Product.css';
 
-const Product = () => {
+const Product = props => {
+    const isSale = true;
+
     return ( 
         <div className='product'>
             <img src="https://mosquito-sklep.pl/30863-tm_large_default/bluza-z-kieszenia-i-kapturem-forest-green-ilm.jpg" alt="" />
-
-            <h3>Bluza z kapturem</h3>
+            {isSale &&<div className='sale'>
+                <span> S </span>
+                <span> A </span>
+                <span> L </span>
+                <span> E </span>
+            </div>}
+            <h3>Bluza z kapturem z kieszenią i spodnie dresowe</h3>
             <p>79.99$</p>
         </div>
      );
